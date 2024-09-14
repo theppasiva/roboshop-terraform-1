ecc0de8209c5db762df8da7772e942c99aba3158
theppasiva <theppasiva@gmail.com>
2024-09-15 00:26:15 +0530

    roboshop-terraform

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -2,6 +2,21 @@ module "mongodb" {
     source = "../../terraform-aws-security-group-1"
     project_name = var.project_name
     environment = var.environment
+    sg_description = "SG for mongodb"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+    #vpc_id = "vpc-0d4deffaa6c175a3b"
     sg_name = "mongodb"
+    #sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
+
+module "catalogue" {
+    source = "../../terraform-aws-security-group-1"
+    project_name = var.project_name
+    environment = var.environment
+    sg_description = "SG for catalogue"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    #vpc_id = "vpc-0d4deffaa6c175a3b"
+    sg_name = "catalogue"
+    #sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
--- 02-sg/variable.tf
+++ 02-sg/variable.tf
@@ -16,3 +16,21 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+variable "mongodb_sg_ingress_rules" {
+  default = [
+    {
+      description = "Allow port 80"
+      from_port   = 80 # 0 means all ports
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow port 443"
+      from_port   = 443 # 0 means all ports
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
