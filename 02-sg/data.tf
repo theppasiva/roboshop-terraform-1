6a0981636706b3d5e5d3a1171193ce62cf23a3a8
theppasiva <theppasiva@gmail.com>
2024-09-15 00:54:03 +0530

    roboshop-terraform

--- 02-sg/data.tf
+++ 02-sg/data.tf
@@ -1,4 +1,4 @@
-data "aws_ssm_parameter" "vpc_id" {
-  name  = "/${var.project_name}/${var.environment}/vpc_id"
+# data "aws_ssm_parameter" "vpc_id" {
+#   name  = "/${var.project_name}/${var.environment}/vpc_id"
 
-}
+# }
