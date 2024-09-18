4d75539cea92db6dc625434b16ec0df8f31cbf83
theppasiva <theppasiva@gmail.com>
2024-09-18 22:45:15 +0530

    roboshop-terraform

--- 03-vpn/main.tf
+++ 03-vpn/main.tf
@@ -5,6 +5,7 @@ module "vpn" {
     instance_type           = "t2.small"
     vpc_security_group_ids  = [data.aws_ssm_parameter.vpn_sg_id.value]
     subnet_id               = data.aws_subnet.selected.id
+    user_data               = file("openvpn.sh")
 
     tags = merge(
         var.common_tags,
